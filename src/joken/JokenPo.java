24d333298b080c69f9e74181476fc4a029dff3be
LEONARDO DE NOVAES VIVI <21276367@CFP127ALUNO.LOCAL>
2021-09-16 15:19:36 -0300

    1.4

--- src/joken/JokenPo.java
+++ src/joken/JokenPo.java
@@ -11,6 +11,7 @@ public class JokenPo {
 
 		int vitoriasrodadasmac = 0;
 		int vitoriasusuario = 0;
+		int empates = 0;
 
 		while (resposta.equals("S") || resposta.equals("s")) {
 			int nJogos;
@@ -40,10 +41,13 @@ public class JokenPo {
 				nJogos = leitor.nextInt();
 			}
 
-			while (nJogos >= cont) {
+			while (nJogos >= cont && !(vitoriasusuario >( nJogos - empates) / 2 || vitoriasrodadasmac > (nJogos - empates) / 2)) {
+
 				Random joken = new Random();
 				int nSorteado = joken.nextInt(3) + 1;
 				int nUsuario;
+
+				nSorteado = 2;
 
 				System.out.println("Escolha uma opção: ");
 				nUsuario = leitor.nextInt();
@@ -52,6 +56,38 @@ public class JokenPo {
 				System.out.println("Numero do Usuario: " + nUsuario);
 
 				cont++;
+
+				if (nUsuario >= 0 && nUsuario <= 3) {
+					if (nSorteado == nUsuario) {
+						System.out.println("Empate!! ");
+						empates++;
+					} else if ((nUsuario == 1 && nSorteado == 3) || (nUsuario == 2 && nSorteado == 1)
+							|| (nUsuario == 3 && nSorteado == 2)) {
+						System.out.println("Usuario Ganhou!! ");
+						vitoriasusuario = vitoriasusuario + 1;
+					} else if (((nUsuario == 3 && nSorteado == 1) || (nUsuario == 1 && nSorteado == 2)
+							|| (nUsuario == 2 && nSorteado == 3))) {
+						System.out.println("Pc Ganhou!! ");
+						vitoriasrodadasmac = vitoriasrodadasmac + 1;
+					}
+				}
+			}
+
+			while (vitoriasrodadasmac == vitoriasusuario) {
+
+				System.out.println("vamos desempatar ? (S/N)");
+				resposta = leitor.next();
+				int nSorteado;
+				int nUsuario;
+				Random joken = new Random();
+				nSorteado = joken.nextInt(3) + 1;
+
+				nSorteado = 2;
+				System.out.println("Escolha uma opção: ");
+				nUsuario = leitor.nextInt();
+
+				System.out.println("Numero sorteado pelo pc: " + nSorteado);
+				System.out.println("Numero do Usuario: " + nUsuario);
 
 				if (nUsuario >= 0 && nUsuario <= 3) {
 					if (nSorteado == nUsuario) {
@@ -65,55 +101,27 @@ public class JokenPo {
 						System.out.println("Pc Ganhou!! ");
 						vitoriasrodadasmac = vitoriasrodadasmac + 1;
 					}
-				}
-				while (vitoriasrodadasmac == vitoriasusuario) {
-					joken = new Random();
-					nSorteado = joken.nextInt(3) + 1;
 
-					System.out.println("Escolha uma opção: ");
-					nUsuario = leitor.nextInt();
-
-					System.out.println("Numero sorteado pelo pc: " + nSorteado);
-					System.out.println("Numero do Usuario: " + nUsuario);
-
-					cont++;
-
-					System.out.println("vamos desempatar ? (S/N)");
-					resposta = leitor.next();
-					if (nUsuario >= 0 && nUsuario <= 3) {
-						if (nSorteado == nUsuario) {
-							System.out.println("Empate!! ");
-						} else if ((nUsuario == 1 && nSorteado == 3) || (nUsuario == 2 && nSorteado == 1)
-								|| (nUsuario == 3 && nSorteado == 2)) {
-							System.out.println("Usuario Ganhou!! ");
-							vitoriasusuario = vitoriasusuario + 1;
-						} else if (((nUsuario == 3 && nSorteado == 1) || (nUsuario == 1 && nSorteado == 2)
-								|| (nUsuario == 2 && nSorteado == 3))) {
-							System.out.println("Pc Ganhou!! ");
-							vitoriasrodadasmac = vitoriasrodadasmac + 1;
-						}
-
-					} else {
-						System.out.println("O Numero que você digitou não é elegivel");
-					}
-
+				} else {
+					System.out.println("O Numero que você digitou não é elegivel");
 				}
 
 			}
-			System.out.println("Jogar novamente(S/N):");
-			resposta = leitor.next();
 
+			System.out.println("--------------------------------------------------------");
 			if (vitoriasrodadasmac > vitoriasusuario) {
 				System.out.println("maquina ganhou");
 			} else {
 				System.out.println("usuario ganhou");
 			}
-
-			leitor.close();
-
-			System.out.println("*****fim*****");
+			System.out.println("--------------------------------------------------------");
+			vitoriasrodadasmac = 0;
+			vitoriasusuario = 0;
+			System.out.println("Jogar novamente(S/N):");
+			resposta = leitor.next();
 
 		}
-
+		leitor.close();
+		System.out.println("*****fim*****");
 	}
 }
